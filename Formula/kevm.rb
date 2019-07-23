853b4d66e97b0aa03aae727855038228f65754de
Dwight Guth <dwight.guth@runtimeverification.com>
2019-07-23 23:57:15 +0000

    set libff compiler

--- Formula/kevm.rb
+++ Formula/kevm.rb
@@ -6,13 +6,12 @@ class Kevm < Formula
   depends_on "kframework" => :build
   depends_on "protobuf" => :build
   depends_on "pandoc" => :build
-  depends_on "llvm@6" => :build
   depends_on "cmake" => :build
   depends_on "jemalloc"
   depends_on "cryptopp"
 
   def install
-    system "make", "K_RELEASE=/usr/lib/kframework", "build-node"
+    system "make", "K_RELEASE=/usr/lib/kframework", "LIBFF_CC=clang", "LIBFF_CXX=clang++", "build-node"
     system "make", "K_RELEASE=/usr/lib/kframework", "INSTALL_PREFIX=#{prefix}", "install"
   end
 
