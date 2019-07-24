6681f4bc369357b1069212d7a92e0c9ac3ddcfc8
Dwight Guth <dwight.guth@runtimeverification.com>
2019-07-24 15:44:36 +0000

    update to beta 3

--- Formula/kevm.rb
+++ Formula/kevm.rb
@@ -1,8 +1,8 @@
 class Kevm < Formula
   desc "KEVM 1.0.0"
   homepage ""
-  url "https://github.com/kframework/evm-semantics/releases/download/v1.0.0b2/kevm-1.0.0b2.tar.gz"
-  sha256 "1462e74411e132576d5c6fbf6440ef54e3820b836892927e9362c883af446769"
+  url "https://github.com/kframework/evm-semantics/releases/download/v1.0.0b3/kevm-1.0.0b3.tar.gz"
+  sha256 "6964a200f109078f3558b25878ff9753476b7c406ecddfda94f4bec4b2b36d46"
   depends_on "kframework" => :build
   depends_on "protobuf" => :build
   depends_on "pandoc" => :build
