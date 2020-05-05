38ef8711f74a009746ef514b076e5d8110be3bea
RV Jenkins <admin@runtimeverification.com>
2020-05-05 11:35:19 -0700

    Update kframework to fae5258: part 2

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -1,8 +1,8 @@
 class Kframework < Formula
   desc "K Framework Tools 5.0"
   bottle do
-    root_url "https://github.com/kframework/k/releases/download/v5.0.0-4a011c9/"
-    sha256 "8c58cd6e3e851197fab1e7c4060298fcb84fbf98fbdde65200f13a2a59d7f126" => :mojave
+    root_url "https://github.com/kframework/k/releases/download/v5.0.0-fae5258/"
+    sha256 "22f635ab0e244dfe6642e39ced1b831a31e42bf5ab004dc94a98a89ebbebff24" => :mojave
   end
 
   homepage ""
