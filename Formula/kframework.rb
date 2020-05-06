21b7cdb036f0dc179b40bfea26f6dc39dbc2ead0
Jenkins <jenkins-slave@Mac-mini.local>
2020-05-06 10:12:02 -0700

    Update kframework to db11f1f: part 3

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -6,7 +6,7 @@ class Kframework < Formula
   end
 
   homepage ""
-  url "file:////Users/jenkins-slave/workspace/k_master/homebrew-k/../kframework-5.0.0-src.tar.gz"
+  url "https://github.com/kframework/k/releases/download/v5.0.0-db11f1f/kframework-5.0.0-src.tar.gz"
   sha256 "739597eb43fcbec615691b8332114228fd48dcfb1324017918fc9b8bff18c0a7"
   depends_on "maven" => :build
   depends_on "cmake" => :build
