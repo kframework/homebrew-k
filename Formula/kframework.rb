6a227b9f21c9bde47aa954312278fbcf3502236c
RV Jenkins <admin@runtimeverification.com>
2020-07-21 10:23:54 -0700

    Update kframework to bbc70cb: part 1

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -6,8 +6,8 @@ class Kframework < Formula
   end
 
   homepage ""
-  url "https://github.com/kframework/k/releases/download/v5.0.0-d10c70c/kframework-5.0.0-src.tar.gz"
-  sha256 "97180e84406653169228d519cda73c21c1cbd7cd86233e37ac2a4746927153f4"
+  url "file:////Users/jenkins-slave/workspace/k_master/homebrew-k/../kframework-5.0.0-src.tar.gz"
+  sha256 "69a42336b46c8ed81fdea8c83ef14591cf799d93b3b19868b7716909892da9fc"
   depends_on "maven" => :build
   depends_on "cmake" => :build
   depends_on "boost" => :build
