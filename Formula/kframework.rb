b43e5cda11a8c5acd0a8178297507f59163013b0
RV Jenkins <admin@runtimeverification.com>
2020-11-09 14:03:47 -0800

    Update kframework to cb6d15b: part 1

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -7,8 +7,8 @@ class Kframework < Formula
   end
 
   homepage ""
-  url "file:////Users/jenkins-slave/workspace/k_PR-1618/homebrew-k/../kframework-5.0.0-src.tar.gz"
-  sha256 "caaf431da9395df1a5b644ffcf648fac0cd0be5dbb87517ec18c3cea72f22541"
+  url "file:////Users/jenkins-slave/workspace/k_master/homebrew-k/../kframework-5.0.0-src.tar.gz"
+  sha256 "bdad7239c382a4420a922cc4bd3755e25252c19c52aeb1a18b238ebfa2c03154"
   depends_on "maven" => :build
   depends_on "cmake" => :build
   depends_on "boost" => :build
