827cf4df5bf146c6db0b371c95c86053da954591
Jenkins <jenkins-slave@Mac-mini.local>
2021-03-02 15:02:11 -0800

    Update kframework to 863727b: part 3

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -7,7 +7,7 @@ class Kframework < Formula
   end
 
   homepage ""
-  url "file:////Users/jenkins-slave/workspace/k_master/homebrew-k/../kframework-5.0.0-src.tar.gz"
+  url "https://github.com/kframework/k/releases/download/v5.0.0-863727b/kframework-5.0.0-src.tar.gz"
   sha256 "43c099e4730e9dd2a6068de8693cf309e47375ec6ec43fdc8fbe7d33290214b1"
   depends_on "maven" => :build
   depends_on "cmake" => :build
