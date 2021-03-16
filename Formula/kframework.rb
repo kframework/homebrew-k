9b4ea2b94ea62c7dedab256d22dd02d1b8796052
Thomas Tuegel <ttuegel@mailbox.org>
2021-03-16 10:24:59 -0500

    kframework.rb: deparallelize stack setup

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -28,6 +28,15 @@ class Kframework < Formula
     ENV["DESTDIR"] = ""
     ENV["PREFIX"] = "#{prefix}"
 
+    # Unset MAKEFLAGS for `stack setup`.
+    # Prevents `install: mkdir ... ghc-7.10.3/lib: File exists`
+    # See also: https://github.com/brewsci/homebrew-science/blob/bb52ecc66b6f9fad4d281342139189ae81d7c410/Formula/tamarin-prover.rb#L27
+    ENV.deparallelize do
+      cd "haskell-backend/src/main/native/haskell-backend" do
+        system "stack", "setup"
+      end
+    end
+
     system "mvn", "package", "-DskipTests", "-Dproject.build.type=FastBuild"
     system "package/package"
   end
