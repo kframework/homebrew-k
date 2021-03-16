f29ecd77fa3d2496d6af942b5824568dab76692c
RV Jenkins <admin@runtimeverification.com>
2021-03-16 08:15:56 -0700

    Update kframework to adf2f2d: part 2

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -1,9 +1,9 @@
 class Kframework < Formula
   desc "K Framework Tools 5.0"
   bottle do
-    root_url "https://github.com/kframework/k/releases/download/v5.0.0-e6f260d/"
-    rebuild 85
-    sha256 mojave: "197bb7bb7058fd0f815c72f39a1f08e18fa65c17763e5b0f4d09951888da6b55"
+    root_url "https://github.com/kframework/k/releases/download/v5.0.0-adf2f2d/"
+    rebuild 86
+    sha256 mojave: "767b5055078c9b6ad917b70324d03fcc1294654dc1d0ffac5bcba9810f690146"
   end
 
   homepage ""
