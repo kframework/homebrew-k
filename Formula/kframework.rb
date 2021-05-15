efeb0048c8ca9633f3e4d28e60b8021c7448d120
RV Jenkins <admin@runtimeverification.com>
2021-05-15 08:06:52 -0700

    Update kframework to 400e731: part 1

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -6,8 +6,8 @@ class Kframework < Formula
   end
 
   homepage ""
-  url "https://github.com/kframework/k/releases/download/v5.1.19/kframework-5.1.19-src.tar.gz"
-  sha256 "f847b5711af0562bc4a32cfbdae8fce779b8b5ab76eadc20141cfe06857b6a0e"
+  url "file:////Users/jenkins-slave/workspace/k_release/homebrew-k/../kframework-5.1.20-src.tar.gz"
+  sha256 "43311a83e9eb95f1e164fd35949542c7ca2a2882ac809fd68e4146d94c91a3fe"
   depends_on "maven" => :build
   depends_on "cmake" => :build
   depends_on "boost" => :build
