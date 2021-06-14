de7c2a94e8f3d32354f49868ca6753afb0cdc948
RV Jenkins <admin@runtimeverification.com>
2021-06-14 10:56:28 -0700

    Update kframework to 92bc098: part 1

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -7,8 +7,8 @@ class Kframework < Formula
   end
 
   homepage ""
-  url "https://github.com/kframework/k/releases/download/v5.1.61/kframework-5.1.61-src.tar.gz"
-  sha256 "c5cb434e5c0212e00b59ce2027460430f674c67a97139e05682ec4d8e1f90d4a"
+  url "file:////Users/jenkins-slave/workspace/k_release/homebrew-k/../kframework-5.1.62-src.tar.gz"
+  sha256 "27cdf29b6006bdd384ef0de176cf6c2651aca710bd86eb63d9d7c532db1ec642"
   depends_on "maven" => :build
   depends_on "cmake" => :build
   depends_on "boost" => :build
