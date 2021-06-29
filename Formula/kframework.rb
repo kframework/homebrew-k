e584019f14f0752903b595ccb3e4b65a4bb1770d
Dwight Guth <dwight.guth@runtimeverification.com>
2021-06-29 09:39:19 -0700

    fix style issues

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -1,32 +1,31 @@
 class Kframework < Formula
   desc "K Framework Tools 5.0"
+  homepage ""
+  url "https://github.com/kframework/k/releases/download/v5.1.64/kframework-5.1.64-src.tar.gz"
+  sha256 "f37171c1f1ff507d09ba0f59ae80c4fe3d56c6e6e071ce911f4b0862df329801"
   bottle do
     root_url "https://github.com/kframework/k/releases/download/v5.1.64/"
     rebuild 21
     sha256 mojave: "e5a2c48afef914ec08354ee6ee690aafef8df934eb5224e76398b367391f9b7d"
   end
-
-  homepage ""
-  url "https://github.com/kframework/k/releases/download/v5.1.64/kframework-5.1.64-src.tar.gz"
-  sha256 "f37171c1f1ff507d09ba0f59ae80c4fe3d56c6e6e071ce911f4b0862df329801"
-  depends_on "maven" => :build
-  depends_on "cmake" => :build
   depends_on "boost" => :build
+  depends_on "cmake" => :build
   depends_on "haskell-stack" => :build
-  depends_on "libyaml"
-  depends_on "jemalloc"
-  depends_on "llvm@8"
-  depends_on "gmp"
-  depends_on "mpfr"
-  depends_on "z3"
+  depends_on "maven" => :build
   depends_on "pkg-config" => :build
   depends_on "bison"
   depends_on "flex"
+  depends_on "gmp"
+  depends_on "jemalloc"
+  depends_on "libyaml"
+  depends_on "llvm@8"
+  depends_on "mpfr"
+  depends_on "z3"
 
   def install
     ENV["SDKROOT"] = MacOS.sdk_path
     ENV["DESTDIR"] = ""
-    ENV["PREFIX"] = "#{prefix}"
+    ENV["PREFIX"] = prefix.to_s
 
     # Unset MAKEFLAGS for `stack setup`.
     # Prevents `install: mkdir ... ghc-7.10.3/lib: File exists`
