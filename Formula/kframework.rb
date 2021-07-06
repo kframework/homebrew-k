91f776d28a4044294c7bf57aa7444c980d759c7e
RV Jenkins <admin@runtimeverification.com>
2021-07-06 11:25:24 -0700

    Update kframework to cdb7cad: part 2

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -4,9 +4,9 @@ class Kframework < Formula
   url "file:////Users/jenkins-slave/workspace/k_release/homebrew-k/../kframework-5.1.84-src.tar.gz"
   sha256 "fb7d5d3195a431a05681083efebfb710a1f24755cc90419ce217009fa58dbb24"
   bottle do
-    root_url "https://github.com/kframework/k/releases/download/v5.1.82/"
-    rebuild 27
-    sha256 mojave: "ed3f966c643556afb53eae9547a221d84fad490a07acee3a68d7e4076ef7bde5"
+    root_url "https://github.com/kframework/k/releases/download/v5.1.84/"
+    rebuild 28
+    sha256 mojave: "02d67cd2b793571543e5914d8638fb94c5fdf0b87ab13c1ba07146f9a92dcfe4"
   end
   depends_on "boost" => :build
   depends_on "cmake" => :build
