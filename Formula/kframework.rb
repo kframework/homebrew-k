58d630029ede512c964d3975ebe37e76bb160ab4
Jenkins <jenkins-slave@Mac-mini.local>
2021-08-24 11:43:23 -0700

    Update kframework to d9bd31c: part 3

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -1,7 +1,7 @@
 class Kframework < Formula
   desc "K Framework Tools 5.0"
   homepage ""
-  url "file:////Users/jenkins-slave/workspace/K_release/homebrew-k/../kframework-5.1.154-src.tar.gz"
+  url "https://github.com/kframework/k/releases/download/v5.1.154/kframework-5.1.154-src.tar.gz"
   sha256 "e45f7679530f1324cfe0ecd6cf77dbbe9ac94fea0827e6d78a37f62e56300d88"
   bottle do
     root_url "https://github.com/kframework/k/releases/download/v5.1.154/"
