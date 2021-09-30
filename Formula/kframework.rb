9eff970bd23d0d6a392b679360fc053a3b90aea1
Dwight Guth <dwight.guth@runtimeverification.com>
2021-09-30 16:49:56 -0500

    Add missing libiconv dependency (#8)

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -18,6 +18,7 @@ class Kframework < Formula
   depends_on "gmp"
   depends_on "jemalloc"
   depends_on "libyaml"
+  depends_on "libiconv"
   depends_on "llvm@12"
   depends_on "mpfr"
   depends_on "z3"
