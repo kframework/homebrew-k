76ed0af31405539f61b3d521f5dd4b579041ecfe
RV Jenkins <admin@runtimeverification.com>
2021-11-04 03:13:29 -0700

    Update kframework to 961fda7: part 2

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -4,9 +4,9 @@ class Kframework < Formula
   url "file:////Users/jenkins-slave/workspace/K_release/homebrew-k/../kframework-5.1.244-src.tar.gz"
   sha256 "2483cfc6dbf4ee99ea36cd98c80ff3a6d31c1b4f3b75e8cc5a54ba53aa14ec98"
   bottle do
-    root_url "https://github.com/kframework/k/releases/download/v5.1.243/"
-    rebuild 91
-    sha256 big_sur: "1c7b4c491875c1565a8779e51ba28064d9c851cb7ba80327815e7156d3728b2d"
+    root_url "https://github.com/kframework/k/releases/download/v5.1.244/"
+    rebuild 92
+    sha256 big_sur: "ea9cff4dbd677488fceed6a91be5a3b88b4c82166722fc69b374c7437d9effe9"
   end
   depends_on "boost" => :build
   depends_on "cmake" => :build
