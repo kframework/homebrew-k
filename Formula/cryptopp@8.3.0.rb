1fcda7b28248c304ab983bc3f3d28a04d8758e6a
Bruce Collie <brucecollie82@gmail.com>
2022-03-01 07:50:31 +0000

    Remove use of disabled option (#14)

--- Formula/cryptopp@8.3.0.rb
+++ Formula/cryptopp@8.3.0.rb
@@ -3,13 +3,17 @@ class CryptoppAT830 < Formula
   homepage "https://www.cryptopp.com/"
   url "https://github.com/weidai11/cryptopp/archive/CRYPTOPP_8_3_0.tar.gz"
   sha256 "63304c6f943f435a4e385273e15abb69cff3b85a44663150bf5a6069b84abd43"
-  # https://cryptopp.com/wiki/Config.h#Options_and_Defines
-  bottle :disable, "Library and clients must be built on the same microarchitecture"
+
+  pour_bottle? do
+    false
+  end
+
   def install
     system "make", "shared", "all", "CXX=#{ENV.cxx}"
     system "./cryptest.exe", "v"
     system "make", "install", "PREFIX=#{prefix}"
   end
+
   test do
     (testpath/"test.cpp").write <<~EOS
       #include <cryptopp/sha.h>
