13897c85d105e32cf684f3403b7958360c24b286
Bruce Collie <brucecollie82@gmail.com>
2022-03-28 18:18:08 +0100

    Bump LLVM version required to 13 (#16)

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -18,7 +18,7 @@ class Kframework < Formula
   depends_on "gmp"
   depends_on "jemalloc"
   depends_on "libyaml"
-  depends_on "llvm@12"
+  depends_on "llvm@13"
   depends_on "mpfr"
   depends_on "openjdk"
   depends_on "z3"
