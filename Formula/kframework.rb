00b7650b4f973fdfacf9f9fa17578d66bfbd45a9
runner <runner@Mac-1656026780178.local>
2022-06-23 23:45:22 +0000

    Update brew package version

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -1,12 +1,12 @@
 class Kframework < Formula
   desc "K Framework Tools 5.0"
   homepage ""
-  url "https://github.com/runtimeverification/k/releases/download/v5.3.80/kframework-5.3.80-src.tar.gz"
-  sha256 "acdb389b90fb9cb9d75a228c7a38acbde281c3fdf9b0d81267526a38bb90f950"
+  url "https://github.com/runtimeverification/k/releases/download/v5.3.82/kframework-5.3.82-src.tar.gz"
+  sha256 "e05dc9ef4f3e2b2f4ed41991b767ddd0f65d003490435aed030b18b2fabdc845"
   bottle do
-    root_url "https://github.com/runtimeverification/k/releases/download/v5.3.80/"
-    rebuild 209
-    sha256 big_sur: "02120035fdf8b6da0023ab366823633e50d8fa9666c5eafdf5099bdd3e84fdef"
+    root_url "https://github.com/runtimeverification/k/releases/download/v5.3.82/"
+    rebuild 210
+    sha256 big_sur: "0681e5f0fbfbe239eb35655568ea11a077c9a358cc5b1d4951c45d25b6b21ff6"
   end
   depends_on "boost" => :build
   depends_on "cmake" => :build
